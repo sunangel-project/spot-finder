a733f1990dfa674149c765523420d9761ab3aa81
Fabian Weik <fabian.weik@protonmail.com>
2023-02-18 18:50:00 +0100

    read osm file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+use std::process::exit;
+use std::{io, fs::File};
+use osmpbfreader;
+
+const OSM_DATA_FILE: &str = "data/germany-latest.osm.pbf";
+
+fn get_osm_file_reader() -> io::BufReader<File> {
+    match File::open(OSM_DATA_FILE) {
+        Ok(file) => io::BufReader::new(file),
+        Err(_e) => {
+            // TODO: warning
+
+            exit(1)
+        },
+    }
+}
+
+
 fn main() {
-    println!("Hello, world!");
+    let mut pbf = osmpbfreader::OsmPbfReader::new(get_osm_file_reader());
+    let objs = pbf.get_objs_and_deps(|obj| {
+        obj.is_way() && obj.tags().contains_key("highway")
+    })
+    .unwrap();
+    for (id, obj) in &objs {
+        println!("{:?}: {:?}", id, obj);
+    }
 }
