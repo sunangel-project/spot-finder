96e9654480e475d6b5ff12e7c2f6c16cb773295e
Fabian Weik <fabian.weik@protonmail.com>
2023-02-22 15:16:26 +0100

    receive messages from serach topic

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,20 @@
+use std::future;
+
+use futures_util::stream::StreamExt;
 
 pub mod spot_finder;
 pub mod location;
 
-fn main() {
-    let nc_res = nats::connect("localhost");
+#[tokio::main]
+async fn main() -> Result<(), async_nats::Error> {
+    let connection = async_nats::connect("localhost").await?;
+    let subscriber = connection.subscribe("search".to_string()).await?;
     
-    let res = match nc_res {
-        Ok(nc) => nc.publish("foo", "my message"), // nc.subscribe("foo"),
-        Err(e) => Err(e),
-    };
+    subscriber.for_each(|msg| {
+        println!("{msg:?}");
+
+        future::ready(())
+    }).await;
     
-    match res {
-        Ok(sub) => (), // sub.iter().for_each(|msg| println!("{msg:?}")),
-        Err(e) => println!("{e:?}")
-    }
+    Ok(())
 }
