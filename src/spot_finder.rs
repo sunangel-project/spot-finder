a137e821e118e2120484d35a50b89c20f05a1a87
Fabian Weik <fabian.weik@protonmail.com>
2023-02-26 20:29:58 +0100

    use overpass to allow for more data downloaded

--- src/spot_finder.rs
+++ src/spot_finder.rs
@@ -1,35 +1,43 @@
 use std::io::Cursor;
+
+use anyhow::bail;
 use osm_xml::{OSM, Node};
+use reqwest::StatusCode;
 use serde::{Serialize, Deserialize};
 
 use crate::location::Location;
 
 const LENGTH_DEG: f64 = 111_000.0;
 
-async fn get_osm_data(loc: &Location, rad: u32) -> Result<String, reqwest::Error>  {
+const OVERPASS_URL: &str = "https://lz4.overpass-api.de/api/interpreter";
+
+async fn get_osm_data(loc: &Location, rad: u32) -> Result<String, anyhow::Error>  {
     let rad_deg = 1.2 * (rad as f64 / LENGTH_DEG);
 
-    let left = loc.lon - rad_deg;
-    let bot = loc.lat - rad_deg;
-    let right = loc.lon + rad_deg;
-    let top = loc.lat + rad_deg;
+    let south = loc.lat - rad_deg;
+    let west = loc.lon - rad_deg;
+    let north = loc.lat + rad_deg;
+    let east = loc.lon + rad_deg;
 
-    let url = format!(
-        "https://www.openstreetmap.org/api/0.6/map?bbox={},{},{},{}",
-        left, bot, right, top,
+    let body = format!(
+        "(node({},{},{},{}); <;); out meta;",
+        south, west, north, east,
     );
     
-    println!("{url}");
-    println!("sending req");
-
-    let response = reqwest::get(url)
-        .await?
-        .text()
-        .await?;
+    let client = reqwest::Client::new();
+    let request = client
+        .post(OVERPASS_URL)
+        .body(body);
+    let response = request.send().await?;
     
-    println!("got some response");
-
-    Ok(response)
+    if response.status() == StatusCode::OK {
+        Ok(response.text().await?)
+    } else {
+        bail!(
+            "overpass returned {}",
+            response.status(),
+        )
+    }
 }
 
 // Spot
@@ -54,9 +62,10 @@ fn is_in_search_area(node: &Node, point: &Location, radius: f64) -> bool {
 } */
 
 pub async fn find_spots(loc: &Location, rad: u32) -> Result<Vec<Spot>, async_nats::Error> {
+    println!("send request");
     let osm_data = get_osm_data(loc, rad).await?;
+    println!("parse data");
     let osm = OSM::parse(Cursor::new(osm_data))?;
-    
     println!("parsed data");
     
     let spots = osm.nodes.iter()
