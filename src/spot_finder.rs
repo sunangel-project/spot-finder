380832439e6993f44ea162e5ee52e45b47cedd66
Fabian Weik <fabian.weik@protonmail.com>
2023-02-26 21:01:55 +0100

    adjust query to only get necessary nodes

--- src/spot_finder.rs
+++ src/spot_finder.rs
@@ -7,22 +7,20 @@ use serde::{Serialize, Deserialize};
 
 use crate::location::Location;
 
-const LENGTH_DEG: f64 = 111_000.0;
-
 const OVERPASS_URL: &str = "https://lz4.overpass-api.de/api/interpreter";
 
 async fn get_osm_data(loc: &Location, rad: u32) -> Result<String, anyhow::Error>  {
-    let rad_deg = 1.2 * (rad as f64 / LENGTH_DEG);
-
-    let south = loc.lat - rad_deg;
-    let west = loc.lon - rad_deg;
-    let north = loc.lat + rad_deg;
-    let east = loc.lon + rad_deg;
-
-    let body = format!(
-        "(node({},{},{},{}); <;); out meta;",
-        south, west, north, east,
+    let body = format!( // TODO: if need more nodes, adjust query
+        "nwr(around:{},{},{})->.all;
+        (
+            node.all[amenity=bench];
+            node.all[bench=yes];
+        );
+        out meta;",
+        rad, loc.lat, loc.lon,
     );
+    
+    println!("{body}");
     
     let client = reqwest::Client::new();
     let request = client
@@ -52,33 +50,23 @@ pub struct Spot {
 
 fn is_bench(n: &&Node) -> bool {
     n.tags.iter().any(|t|
-        t.key == "bench"
+        (t.key == "amenity" && t.key == "bench")
+        || t.key == "bench"
     )
 }
 
-/*
-fn is_in_search_area(node: &Node, point: &Location, radius: f64) -> bool {
-    point.dist(&Location::from(node)) < radius
-} */
-
 pub async fn find_spots(loc: &Location, rad: u32) -> Result<Vec<Spot>, async_nats::Error> {
-    println!("send request");
     let osm_data = get_osm_data(loc, rad).await?;
-    println!("parse data");
     let osm = OSM::parse(Cursor::new(osm_data))?;
-    println!("parsed data");
     
     let spots = osm.nodes.iter()
     .map(|(_, node)| node)
     .filter(is_bench)
-    .filter(|_| true) // TODO: filter search area
     .map(|node| Spot {
         r#type: "bench".to_string(),
         loc: Location::from(node),
         dir: None,
     }).collect();
-    
-    println!("filtered");
 
     Ok(spots)
 }
